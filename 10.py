d52edec180aadbc7919e7c66319e3d9a930af6d5
Nozomi-Tojo <62291919+Nozomi-Tojo@users.noreply.github.com>
2022-06-01 12:42:11 +0900

    Update 10.py

--- 10.py
+++ 10.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 df = pd.read_table('./popular-names.txt', header=None, sep='\t', names=['name', 'sex', 'number', 'year'])
 print(len(df))
+
+#2780
+
+# 確認
+#!wc -l ./popular-names.txt
+#2780 ./popular-names.txt
